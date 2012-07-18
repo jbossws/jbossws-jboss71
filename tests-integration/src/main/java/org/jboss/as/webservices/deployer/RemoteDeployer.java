4c5a47b228c79c43435c1928227890a8ae818661
richard.opalka@jboss.com <richard.opalka@jboss.com>
2012-07-18 13:21:30 +0000

    [JBWS-3484] refactoring AS 712 integration layer

--- tests-integration/src/main/java/org/jboss/as/webservices/deployer/RemoteDeployer.java
+++ tests-integration/src/main/java/org/jboss/as/webservices/deployer/RemoteDeployer.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.webservices.tests;
+package org.jboss.as.webservices.deployer;
 
 import java.io.IOException;
 import java.net.InetAddress;
